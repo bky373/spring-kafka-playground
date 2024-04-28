daf928d5fc5d4311d10c2aea11d865d451aaaae6
bky373 <bo373@naver.com>
2024-04-28 15:38:50 +0900

    Init spring-kafka-playground

---
+++ src/main/java/com/bky373/springkafkaplayground/SpringKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.bky373.springkafkaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bky373/springkafkaplayground/SpringKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bky373.springkafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
