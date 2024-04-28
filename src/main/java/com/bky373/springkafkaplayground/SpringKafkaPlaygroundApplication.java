447c5307f78a31e543e558a5b9ec13dd67627b9d
bky373 <bo373@naver.com>
2024-04-28 15:45:20 +0900

    Practice a Quick Tour
    https://docs.spring.io/spring-kafka/reference/quick-tour.html

--- src/main/java/com/bky373/springkafkaplayground/SpringKafkaPlaygroundApplication.java
+++ src/main/java/com/bky373/springkafkaplayground/SpringKafkaPlaygroundApplication.java
@@ -1,7 +1,13 @@
 package com.bky373.springkafkaplayground;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringKafkaPlaygroundApplication {
@@ -10,4 +16,23 @@ public class SpringKafkaPlaygroundApplication {
         SpringApplication.run(SpringKafkaPlaygroundApplication.class, args);
     }
 
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                           .partitions(10)
+                           .replicas(1)
+                           .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println("in = " + in);
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("topic1", "test");
+        };
+    }
 }
